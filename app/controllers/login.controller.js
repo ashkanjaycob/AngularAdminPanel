e04154775e43f5dbc1a8c1ecdd25454e3169272b
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2025-01-13 16:35:27 +0330

    Added Phone & Password Validation

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,13 +1,23 @@
-app.controller("LoginController", function ($scope, $http) {
+app.controller("LoginController", function ($scope, $http, $location) {
   $scope.credentials = {
     username: "",
     password: "",
   };
 
+  $scope.validateAndLogin = function () {
+    if (!/^\d{11}$/.test($scope.credentials.username)) {
+      alert("شماره تلفن باید ۱۰ رقم باشد");
+      return;
+    }
+    if ($scope.credentials.password.length <= 3) {
+      alert("رمز عبور باید بیشتر از ۳ کاراکتر باشد");
+      return;
+    }
+    // If validations pass, proceed with login
+    $scope.login();
+  };
+
   $scope.login = function () {
-    //check if the credentials are valid
-    console.log($scope.credentials);
-    
     $http({
       method: "POST",
       url: "/api/user/create-session",
@@ -21,6 +31,7 @@ app.controller("LoginController", function ($scope, $http) {
     }).then(
       function successCallback(response) {
         console.log("Login successful:", response.data);
+        $location.path("/");
       },
       function errorCallback(error) {
         console.error("Login failed:", error.data);
